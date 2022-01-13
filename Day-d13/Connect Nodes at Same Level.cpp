7d3123c9773d115238355be5642e0165de014bd5
Sree Sayi Hrudai <63182223+hrudai2002@users.noreply.github.com>
2022-01-13 18:27:54 +0530

    Connect Nodes at Same Level ✅

---
+++ Day-d13/Connect Nodes at Same Level.cpp
@@ -0,0 +1,199 @@
+/* 
+
+Problem - https://practice.geeksforgeeks.org/problems/connect-nodes-at-same-level/1/ 
+Submission - https://practice.geeksforgeeks.org/viewSol.php?subId=3a9efcd1bdb876ad5d3d7312bc6ea460&pid=700184&user=sayihrudai
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+    Node* nextRight;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    temp->nextRight = NULL;
+    
+    return temp;
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+void connect(struct Node *p);
+
+/* Helper function that allocates a new node with the
+   given data and NULL left and right pointers. */
+
+
+void printSpecial(Node *root)
+{
+   if (root == NULL)
+     return;
+
+   Node* next_root=NULL;
+
+   while (root != NULL)
+   {
+      cout<< root->data<<" ";
+
+      if( root->left && (!next_root) )
+        next_root = root->left;
+      else if( root->right && (!next_root)  )
+        next_root = root->right;
+
+      root = root->nextRight;
+   }
+   
+   printSpecial(next_root);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+       return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+ // } Driver Code Ends
+/* struct Node
+{
+  int data;
+  Node *left,  *right;
+  Node *nextRight;  // This has garbage value in input trees
+}; */
+
+
+class Solution
+{
+    public:
+   
+    void connect(Node *root)
+    {
+       queue<Node*> q; 
+       q.push(root);
+       q.push(NULL);
+       
+       while(!q.empty()) {
+           struct Node* t1 = q.front(); 
+           q.pop();
+           if(t1 == NULL) {
+               if(!q.empty()) q.push(NULL);
+           }
+           else {
+                t1->nextRight = q.front(); 
+                if(t1->left) {
+                       q.push(t1->left);
+                 }
+                if(t1->right) {
+                       q.push(t1->right);
+                }
+           }
+        }
+    }    
+      
+};
+
+
+
+
+// { Driver Code Starts.
+
+
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d\n", &t);
+  while (t--)
+  {
+     string s;
+     getline(cin, s);
+     Node* root = buildTree(s);
+        
+    Solution obj;
+     obj.connect(root);
+     printSpecial(root);
+     cout<<endl;
+     inorder(root);
+     cout<<endl;
+  }
+  return 0;
+}
+  // } Driver Code Ends
